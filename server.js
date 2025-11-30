f6461e11ee45d8beea86427de2fb01320d364020
EDWARD10201 <seiu5002@gmail.com>
2025-12-01 01:14:17 +0900

    mysqlに接続

--- server.js
+++ server.js
@@ -4,11 +4,41 @@ const mysql = require("mysql2");
 const post = 3000;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded())
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "practice_1"
+});
+
+db.connect((err) => {
+    if(err){
+        console.error(`mysqlに接続できませんでした${err}`);
+        return;
+    } else {
+        console.log("mysql接続成功")
+    }
+})
+
+
 
 app.get("/", (req,res) => {
     res.render("index");
 })
 
+
+app.post("/posts", (req, res) => {
+    const {name} = req.body;
+    const sql = "INSERT INTO users (name) VALUES(?)";
+
+    db.query(sql, [name], (err, result) => {
+        if(err){ console.error(`データ取得失敗${err}`)}
+        return;
+    })
+
+    res.redirect("/");
+})
 
 
 app.listen(post, () => {
