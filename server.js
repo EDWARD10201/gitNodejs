7cc238d335c84dd90feed8bdaf4bdc566825c301
EDWARD10201 <seiu5002@gmail.com>
2025-12-02 23:29:20 +0900

    昨日の追加

--- server.js
+++ server.js
@@ -2,45 +2,86 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 const post = 3000;
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded())
+
+//mysql接続
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "practice_1"
 });
-
 db.connect((err) => {
-    if(err){
-        console.error(`mysqlに接続できませんでした${err}`);
+    if (err) {
+        console.error("mysql接続失敗", err);
         return;
     } else {
-        console.log("mysql接続成功")
+        console.log("mysql接続成功");
     }
-})
+});
 
 
 
-app.get("/", (req,res) => {
-    res.render("index");
+
+
+
+
+
+app.get("/", (req, res) => {
+    const sql = "SELECT * FROM users ORDER BY id DESC";
+    db.query(sql, (err, result) => {
+        if(err) {
+            console.error("データ取得失敗", err);
+            return;
+        } else {
+            res.render("index", {users: result});
+        }
+    })
 })
 
 
 app.post("/posts", (req, res) => {
     const {name} = req.body;
-    const sql = "INSERT INTO users (name) VALUES(?)";
-
+    const sql = "INSERT INTO users (name) VALUES (?)";
     db.query(sql, [name], (err, result) => {
-        if(err){ console.error(`データ取得失敗${err}`)}
-        return;
+        if (err) {
+            console.error("データ挿入失敗", err);
+            return;
+        } else {
+            res.redirect("/");
+        }
     })
-
-    res.redirect("/");
 })
 
 
+app.get("/pokemon/:start/:end", async (req, res) => {
+    const {start, end} = req.params;
+    let pokemons= [];
+
+    for( let i = Number(start); i < Number(end); i++) {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        const data = await response.json();
+        pokemons.push({
+            id: data.id,
+            name: data.name,
+            image: data.sprites.front_default
+        });
+    }
+
+            res.render("pokemon", { pokemons });
+
+    
+
+});
+
+//サーバー起動
 app.listen(post, () => {
-    console.log("サーバー起動")
-} );
+    console.log(`サーバーが起動しました:${post}`);
+})
+
+
+
+
+
+
